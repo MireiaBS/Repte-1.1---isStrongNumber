fb068015140fd55baee41fe5e89b0596ee0c2ba6
MireiaBS <mireia__24@hotmail.com>
2022-07-02 19:11:15 +0200

    Start

---
+++ isStrongNumber.js
@@ -0,0 +1,28 @@
+function isStrongNumber(num) {
+
+ /*Un nombre fort és el nombre que la suma del factorial dels seus dígits és igual al número mateix.
+ 
+ Per exemple:
+ 
+ 145 és fort, ja que 1! + 4! + 5! = 1 + 24 + 120 = 145
+ El teu objectiu és construir una funció que ens digui si un número és fort o no i torni:
+ 
+ STRONG!!!! si ho és
+ 
+ Not Strong !! sí no ho és
+ 
+ Notes
+ Els números que es passen són sempre positius
+ 
+ La funció torna un string
+ 
+ Lliurament únicament la classe amb la funció / funcions necessàries.
+ 
+ La funció isStrongNumber sempre ha d'existir dins de la classe i és la que es trucarà per a les proves. isStrongNumber(1) ==> return "STRONG!!!!"
+     isStrongNumber(123) ==> return "Not Strong !!"
+     isStrongNumber(2) ==>  return "STRONG!!!!"
+     isStrongNumber(150) ==> return "Not Strong !!" */
+
+
+    return ""
+}
